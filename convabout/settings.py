910ea8ee2aeb3fa4850998be6a08fe9033e06c14
Andrii Savchenko <andrsvchnko@gmail.com>
2020-06-24 10:55:18 +0300

    Change redis url env. variable name

--- convabout/settings.py
+++ convabout/settings.py
@@ -199,7 +199,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('CONVABOUT_REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
